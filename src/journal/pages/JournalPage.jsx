7400cda93d8456d2f301b1de730c574ee7179fb2
Dante Tárraga <dante.tarragau@gmail.com>
2024-07-23 12:51:40 -0500

    feat: Add fileUpload and loadNotes helper functions

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,16 +1,24 @@
 import { AddOutlined } from '@mui/icons-material'
 import { IconButton } from '@mui/material'
-import { NothingSelectedView } from '../views'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { startNewNote } from '../../store/journal'
 import JournalLayout from '../layout/JournalLayout'
+import { NoteView, NothingSelectedView } from '../views'
 
 const JournalPage = () => {
+  const dispatch = useDispatch()
+  const { isSaving, active } = useSelector((state) => state.journal)
+
+  const onClickNewNote = () => dispatch(startNewNote())
+
   return (
     <JournalLayout>
-      <NothingSelectedView />
-
-      {/* <NoteView /> */}
+      {active ? <NoteView /> : <NothingSelectedView />}
 
       <IconButton
+        onClick={onClickNewNote}
+        disabled={isSaving}
         size='large'
         sx={{
           color: 'white',
